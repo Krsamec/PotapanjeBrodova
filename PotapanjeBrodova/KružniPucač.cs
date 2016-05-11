302eff2e2e6bafce1ab2bcf53895b0034eb2393f
Krsamec <krsamec@vsite.hr>
2016-05-11 18:31:16 +0200

    hh

--- PotapanjeBrodova/KružniPucač.cs
+++ PotapanjeBrodova/KružniPucač.cs
@@ -5,7 +5,19 @@ using System.Text;
 
 namespace PotapanjeBrodova
 {
-    class KružniPucač
+    public class KružniPucač : IPucač
     {
+        public KružniPucač(Polje prvoPogođeno, Mreža mreža)
+        {
+            this.prvoPogođeno = prvoPogođeno;
+            this.mreža = mreža;
+        }
+
+        public Polje UputiPucanj()
+        {
+            throw new NotImplementedException();
+        }
+        Polje prvoPogođeno;
+        Mreža mreža;
     }
 }
--- UnitTests/TestTopništva.cs
+++ UnitTests/TestTopništva.cs
@@ -1,17 +1,27 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PotapanjeBrodova;
+using System.Linq;
 
 namespace UnitTests
 {
+   
     [TestClass]
     public class TestTopništva
     {
+        int[] duljineBrodova = { 1, 2, 3 };
         [TestMethod]
         public void Topništvo_PočetnaTaktikaGađanjaJeNapipavanje()
         {
-            Topništvo t = new Topništvo();
+            Topništvo t = new Topništvo(10, 10, duljineBrodova);
             Assert.AreEqual(TaktikaGađanja.Napipavanje, t.TrenutnaTaktika);
         }
     }
+}
+[TestMethod]
+public void Topništvo_NapipavanjeDajeBilokojePoljeNaPraznojMreži()
+{
+    Topništvo t = new Topništvo(10, 10, duljineBrodova);
+    Assert.IsTrue(new Mreža(10, 10).DajSlobodnaPolja().Contains(t.UputiPucanj()));
+
 }
